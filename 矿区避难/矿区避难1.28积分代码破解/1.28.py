440f686a3812448441dbef5b07528130e2e2bd3b
FGFW <yishanju@gmail.com>
2015-11-05 21:10:38 +0800

    ಠ_ಠ

--- 矿区避难/矿区避难1.28积分代码破解/1.28.py
+++ 矿区避难/矿区避难1.28积分代码破解/1.28.py
@@ -1,1 +1,15 @@
+MR="Holy_xXx"
 az="ZXCVBNM1234567890ASDFGHJKLQWERTYUIOP!@#"
+gid=range(15,0,-1)
+
+#for lid in gid:
+#    lid=8
+TO=['','']    
+TO[0]=999
+TO[1]=100
+UO=2
+XT=""
+XT=XT+"999"+"-"
+XT=XT+"100"+"-"
+for i in UO:
+    XT=XT+
